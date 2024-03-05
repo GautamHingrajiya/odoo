323a90018666b6d8ca6e99f041e92761782d5f37
GautamHingrajiya <gautamhingrajiya@gmail.com>
2024-03-05 22:46:46 +0530

    [17.0][ADD] Initial Commit

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -6,13 +6,13 @@ class HospitalPetient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Petient"
 
-    name = fields.Char('Patient Name', tracking=True)
+    name = fields.Char('Patient Name', tracking=True, required=True)
     age = fields.Integer('Patient Age', compute='_compute_age', tracking=True)
-    ref = fields.Char('Reference', tracking=True)
+    ref = fields.Char('Reference', tracking=True, required=True)
     gender = fields.Selection([('male', 'Male'), 
                                ('female', "Female"),
-                               ('other', 'Other')], 'Patient Gender', tracking=True)
-    dob = fields.Date('Date of Birth')
+                               ('other', 'Other')], 'Patient Gender',required=True, tracking=True)
+    dob = fields.Date('Date of Birth', required=True)
     active = fields.Boolean('Active', default = True, tracking=True)
     # active fields used to create archive and unarchive option in odoo. must need to add active field in views also.
     # ( here active word is reserved for archive and unarchive option)
