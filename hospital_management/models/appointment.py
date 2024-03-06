8a2f0392c5250e8cc172852da646b06324c0190f
GautamHingrajiya <gautamhingrajiya@gmail.com>
2024-03-06 19:08:19 +0530

    [17.0][ADD] Initial Commit

--- hospital_management/models/appointment.py
+++ hospital_management/models/appointment.py
@@ -11,3 +11,26 @@ class HospitalAppointment(models.Model):
     appointment_time = fields.Datetime('Appointment Time', required=True, default=fields.Datetime.now())
     booking_date = fields.Date('Booking Date', default=fields.Datetime.now())
     gender = fields.Selection('Patient Gender', tracking=True, readonly=False, related="patient_id.gender")
+    prescription = fields.Html('Prescription',traking=True)
+    priority = fields.Selection([('0','Less'),
+                                 ('1','Normal'),
+                                 ('2','Intermediate'),
+                                 ('3','High'),
+                                 ('4','Very High'),
+                                 ('5','Extreme')], 'Priority',traking=True)
+    state = fields.Selection([('draft','Draft'),
+                              ('in_consultation','In Consultation'),
+                              ('done','Done'),
+                              ('cancel','Cancel')], 'status', default='draft',traking=True)
+
+    def action_draft(self):
+        self.state = "draft"
+    
+    def action_in_consultation(self):
+        self.state = "in_consultation"
+
+    def action_done(self):
+        self.state = "done"
+    
+    def action_cancel(self):
+        self.state = "cancel"
